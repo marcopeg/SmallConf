563d9b56b04b8f4b87727686401cb6ab6ecbebf6
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-08-22 12:31:43 +0200

    isomorphic app with routes and redux

--- app/index.js
+++ app/index.js
@@ -1,34 +1,27 @@
-/**
- * Isomorphic Application
- */
-
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
+// import ReactDOM from 'react-dom';
 
-var store = createStore((state, action) => {
-    switch (action.type) {
-        case 'CHANGE_PAGE':
-            return Object.assign({}, state, {
-                activePage: action.activePage
-            });
-    }
-    return state;
-}, {
-    activePage: 'page1',
-    pages: [{
-        id: 'page1',
-        title: 'this is page 1'
-    },{
-        id: 'page2',
-        title: 'this is page3'
-    }]
-});
+import { Router, Route } from 'react-router';
+
+import { createStore } from 'redux';
+import { connect } from 'react-redux';
+import { routerStateReducer, reduxRouteComponent, transitionTo } from 'redux-react-router';
+import { LOCATION_DID_CHANGE } from 'redux-react-router/lib/actionTypes';
+
+
+
+function appReducer(state, action) {
+    return {...state,
+        router: routerStateReducer(state.router, action)
+    };
+}
 
 function changePage(activePage) {
     return {
-        type: 'CHANGE_PAGE',
-        activePage
+        type: LOCATION_DID_CHANGE,
+        payload: {
+            pathname: '/' + activePage
+        }
     }
 }
 
@@ -39,27 +32,60 @@ class Page extends React.Component {
     }
     render() {
         var { id, title } = this.props;
-        return <div><h2>{id}</h2><p>{title}</p></div>;
+        return (
+            <div>
+                <h2>{title}</h2>
+                <p><small>id - {id}</small></p>
+            </div>
+        );
     }
 }
 
 class ActivePage extends React.Component {
     static defaultProps = {
-        activePage: null,
+        activePage: 'home',
         pages: []
     }
     render() {
         var { activePage, pages } = this.props;
         var page = pages.filter(page => page.id === activePage).shift();
+        
         if (page) {
-            return <Page {...page} />
+            return <Page {...page} />;
         } else {
-            return <div>Page not found</div>;
+            return <div>Page not found: {activePage}!</div>;
         }
     }
 }
 
-class AppComponent extends React.Component {
+class RouteInfo extends React.Component {
+    render() {
+        return <div>{this.props.params.pageId}</div>
+    }
+}
+
+class Menu extends React.Component {
+    static defaultProps = {
+        onClick: null
+    };
+    render() {
+        var { onClick } = this.props;
+        return (
+            <p style={{marginTop:20}}>
+                <a href="/" onClick={onClick}>Home</a>
+                <span> | </span>
+                <a href="/page1" onClick={onClick}>Page1</a>
+                <span> | </span>
+                <a href="/page2" onClick={onClick}>Page2</a>
+                <span> | </span>
+                <a href="/page3" onClick={onClick}>Page3</a>
+            </p>
+        );
+    }
+}
+
+@connect(state => state)
+class App extends React.Component {
     navigate = (e) => {
         e.preventDefault();
         var { dispatch } = this.props;
@@ -67,36 +93,67 @@ class AppComponent extends React.Component {
         dispatch(changePage(uri));
     }
     render() {
+        var { pages, params } = this.props;
         return (
             <div className="container">
-                <p style={{marginTop:20}}>
-                    <a href="/page1" onClick={this.navigate}>Page1</a>
-                    <span> | </span>
-                    <a href="/page2" onClick={this.navigate}>Page2</a>
-                    <span> | </span>
-                    <a href="/page3" onClick={this.navigate}>Page3</a>
-                </p>
+                <Menu onClick={this.navigate} />
                 <hr />
-                <ActivePage {...this.props} />
+                <ActivePage 
+                    pages={pages}
+                    activePage={params.pageId} />
+                <hr />
+                <h5>Routed Content <small>{this.props.children}</small></h5>
             </div>
         );
     }
 }
 
-var App = connect(s => s)(AppComponent);
+const localState = {
+    pages: [{
+        id: 'home',
+        title: 'home page'
+    }, {
+        id: 'page1',
+        title: 'Page One'
+    }, {
+        id: 'page2',
+        title: 'Page Two'
+    }]
+};
 
+function makeRoutes(initialState) {
+    const store = createStore(appReducer, localState);
+    
+    const routes = (
+        <Route component={reduxRouteComponent(store)}>
+            <Route path="/" component={App}>
+                <Route path="/:pageId" component={RouteInfo} />
+            </Route>
+        </Route>
+    );
 
-// client side startup
-exports.start = function(initialState, fireBase) {
-    var state = store.getState();
+    return [routes, store];
+}
+
+export function start(initialState) {
+    var BrowserHistory = require('react-router/lib/BrowserHistory').history;
+    var [routes] = makeRoutes(initialState);
+
     React.render((
-        <Provider store={store}>
-            {$=> <App />}
-        </Provider>
+        <Router history={BrowserHistory}>
+            {routes}
+        </Router>
     ), document.getElementById('app'));
-};
+}
 
-// server side rendering
-exports.renderMarkup = function(initialState) {
-    // return React.renderToString(<Page />);
-};
+export function renderMarkup(initialState) {
+    var Location = require('react-router/lib/Location');
+    var [routes] = makeRoutes(initialState);
+    var html;
+    
+    Router.run(routes, new Location('/'), (err, initialState) => {
+        html = React.renderToString(<Router {...initialState}/>);
+    });
+
+    return html; 
+}
